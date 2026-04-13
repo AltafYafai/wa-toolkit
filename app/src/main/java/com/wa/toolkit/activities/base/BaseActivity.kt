70b21d85c3bcba178893537f6dd7ac5967912738
altafyafai <altaf.alu@gmail.com>
2026-04-13 15:22:29 +0530

    UI Redesign: Implement Edge-to-Edge display with transparent system bars

--- app/src/main/java/com/wa/toolkit/activities/base/BaseActivity.kt
+++ app/src/main/java/com/wa/toolkit/activities/base/BaseActivity.kt
@@ -1,14 +1,20 @@
 package com.wa.toolkit.activities.base
 
+import android.graphics.Color
 import android.os.Build
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
 import androidx.preference.PreferenceManager
 import com.wa.toolkit.R
 
 open class BaseActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        window.statusBarColor = Color.TRANSPARENT
+        window.navigationBarColor = Color.TRANSPARENT
+        
         setTheme(R.style.AppTheme)
         
         val prefs = PreferenceManager.getDefaultSharedPreferences(this)
